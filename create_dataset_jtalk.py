28c68ecb8409608c2c10f31fc66a3160a206b8b0
isletennos <isle.tennos@gmail.com>
2022-03-06 00:19:42 +0900

    initial commit

---
+++ create_dataset_jtalk.py
@@ -0,0 +1,76 @@
+import glob
+import sys
+import os
+import pyopenjtalk
+
+def mozi2phone(mozi):
+    text = pyopenjtalk.g2p(mozi)
+    text = "sil " + text + " sil"
+    text = text.replace(' ', '-')
+    return text
+
+def create_dataset(filename):
+    speaker_id = 0
+    textful_dir_list = glob.glob("dataset/textful/*")
+    textless_dir_list = glob.glob("dataset/textless/*")
+    textful_dir_list.sort()
+    textless_dir_list.sort()
+    Correspondence_list = list()
+    output_file_list = list()
+    output_file_list_val = list()
+    output_file_list_textless = list()
+    output_file_list_val_textless = list()
+    for d in textful_dir_list:
+        wav_file_list = glob.glob(d+"/wav/*")
+        lab_file_list = glob.glob(d + "/text/*")
+        wav_file_list.sort()
+        lab_file_list.sort()
+        counter = 0
+        for lab, wav in zip(lab_file_list, wav_file_list):
+            with open(lab, 'r') as f:
+                mozi = f.read().split("\n")
+            print(str(mozi))
+            test = mozi2phone(str(mozi))
+            print(test)
+            print(wav + "|"+ str(speaker_id) + "|"+ test)
+            if counter % 10 != 0:
+                output_file_list.append(wav + "|"+ str(speaker_id) + "|"+ test + "\n")
+            else:
+                output_file_list_val.append(wav + "|"+ str(speaker_id) + "|"+ test + "\n")
+            counter = counter +1
+        Correspondence_list.append(str(speaker_id)+"|"+os.path.basename(d) + "\n")
+        speaker_id = speaker_id + 1
+
+    for d in textless_dir_list:
+        wav_file_list = glob.glob(d+"/*")
+        wav_file_list.sort()
+        counter = 0
+        for wav in wav_file_list:
+            print(wav + "|"+ str(speaker_id) + "|a")
+            if counter % 10 != 0:
+                output_file_list_textless.append(wav + "|"+ str(speaker_id) + "|a" + "\n")
+            else:
+                output_file_list_val_textless.append(wav + "|"+ str(speaker_id) + "|a" + "\n")
+            counter = counter +1
+        Correspondence_list.append(str(speaker_id)+"|"+os.path.basename(d) + "\n")
+        speaker_id = speaker_id + 1
+
+    with open('filelists/' + filename + '_textful.txt', 'w', encoding='utf-8', newline='\n') as f:
+        f.writelines(output_file_list)
+    with open('filelists/' + filename + '_textful_val.txt', 'w', encoding='utf-8', newline='\n') as f:
+        f.writelines(output_file_list_val)
+    with open('filelists/' + filename + '_textless.txt', 'w', encoding='utf-8', newline='\n') as f:
+        f.writelines(output_file_list_textless)
+    with open('filelists/' + filename + '_val_textless.txt', 'w', encoding='utf-8', newline='\n') as f:
+        f.writelines(output_file_list_val_textless)
+    with open('filelists/' + filename + '_Correspondence.txt', 'w', encoding='utf-8', newline='\n') as f:
+        f.writelines(Correspondence_list)
+
+def main(argv):
+    filename = str(sys.argv[1])
+    print(filename)
+    create_dataset(filename)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
