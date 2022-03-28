f6d1e7a009b21fe9908e4d584f74623061495b9d
isletennos <isle.tennos@gmail.com>
2022-03-29 07:18:58 +0900

    debug

--- train_ms.py
+++ train_ms.py
@@ -68,7 +68,7 @@ def run(rank, n_gpus, hps):
   train_sampler = DistributedBucketSampler(
       train_dataset,
       hps.train.batch_size,
-      [32,300,400,500,600,700,800,900,1000],
+      [32,100,550,700,850,1000,1150,1300,2000],
       num_replicas=n_gpus,
       rank=rank,
       shuffle=True)
