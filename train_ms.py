cfa0d2f0ba7189902980948cb2ba139b0394ed5a
Satoh Kiyoshi <satoh@hakuba.jp>
2022-06-02 17:35:23 +0900

    configファイル指定でWindowsローカルでも動くようにする
    others.os_typeに「windows」指定することで並列動作ライブラリをDPを利用しbackendをglooにする

--- train_ms.py
+++ train_ms.py
@@ -12,6 +12,7 @@ from torch.utils.tensorboard import SummaryWriter
 import torch.multiprocessing as mp
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.nn.parallel import DataParallel as DP
 from torch.cuda.amp import autocast, GradScaler
 import datetime
 import pytz
@@ -64,6 +65,14 @@ def main():
 
 def run(rank, n_gpus, hps):
   global global_step
+  
+  if hps.others.os_type == "windows":
+    backend_type = "gloo"
+    parallel = DP
+  else: # Colab
+    backend_type = "nccl"
+    parallel = DDP
+
   if rank == 0:
     logger = utils.get_logger(hps.model_dir)
     logger.info(hps)
@@ -75,7 +84,7 @@ def run(rank, n_gpus, hps):
   if cpu_count > 8:
     cpu_count = 8
 
-  dist.init_process_group(backend='nccl', init_method='env://', world_size=n_gpus, rank=rank)
+  dist.init_process_group(backend=backend_type, init_method='env://', world_size=n_gpus, rank=rank)
   torch.manual_seed(hps.train.seed)
   torch.cuda.set_device(rank)
   train_dataset = TextAudioSpeakerLoader(hps.data.training_files, hps.data)
@@ -117,8 +126,8 @@ def run(rank, n_gpus, hps):
       hps.train.learning_rate, 
       betas=hps.train.betas, 
       eps=hps.train.eps)
-  net_g = DDP(net_g, device_ids=[rank])
-  net_d = DDP(net_d, device_ids=[rank])
+  net_g = parallel(net_g, device_ids=[rank])
+  net_d = parallel(net_d, device_ids=[rank])
 
   logger.info('FineTuning : '+str(hps.fine_flag))
   if hps.fine_flag:
