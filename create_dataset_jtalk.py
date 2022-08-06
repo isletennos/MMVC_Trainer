1e5feca34e7b84a9b45f2e52a931c4f493b7654f
isletennos <isle.tennos@gmail.com>
2022-08-06 15:57:37 +0900

    複数話者の同時学習

--- create_dataset_jtalk.py
+++ create_dataset_jtalk.py
@@ -267,6 +267,52 @@ def create_dataset_character(filename, tid):
         f.writelines(Correspondence_list)
     return 110
 
+def create_dataset_multi_character(filename, file_path):
+    Correspondence_list = list()
+    textless_dir_list = glob.glob("dataset/textless/*")
+    textless_dir_list.sort()
+    output_file_list = list()
+    output_file_list_val = list()
+    output_file_list_textless = list()
+    output_file_list_val_textless = list()
+    with open(file_path, "r") as f:
+        for line in f.readlines():
+            target_dir , sid = line.split("|")
+            sid = sid.rstrip('\n')
+            wav_file_list = glob.glob("dataset/textful/" + target_dir + "/wav/*.wav")
+            lab_file_list = glob.glob("dataset/textful/" + target_dir + "/text/*.txt")
+            wav_file_list.sort()
+            lab_file_list.sort()
+            if len(wav_file_list) == 0:
+                print("Error" + target_dir + "/wav に音声データがありません")
+                exit()
+            counter = 0
+            for lab, wav in zip(lab_file_list, wav_file_list):
+                with open(lab, 'r', encoding="utf-8") as f_text:
+                    mozi = f_text.read().split("\n")
+                print(str(mozi))
+                test = mozi2phone(str(mozi))
+                print(test)
+                print(wav + "|"+ str(sid) + "|"+ test)
+                if counter % 10 != 0:
+                    output_file_list.append(wav + "|"+ str(sid) + "|"+ test + "\n")
+                else:
+                    output_file_list_val.append(wav + "|"+ str(sid) + "|"+ test + "\n")
+                counter = counter +1
+            Correspondence_list.append(str(sid)+"|"+ target_dir + "\n")
+
+    with open('filelists/' + filename + '_textful.txt', 'w', encoding='utf-8', newline='\n') as f:
+        f.writelines(output_file_list)
+    with open('filelists/' + filename + '_textful_val.txt', 'w', encoding='utf-8', newline='\n') as f:
+        f.writelines(output_file_list_val)
+    with open('filelists/' + filename + '_textless.txt', 'w', encoding='utf-8', newline='\n') as f:
+        f.writelines(output_file_list_textless)
+    with open('filelists/' + filename + '_val_textless.txt', 'w', encoding='utf-8', newline='\n') as f:
+        f.writelines(output_file_list_val_textless)
+    with open('filelists/' + filename + '_Correspondence.txt', 'w', encoding='utf-8', newline='\n') as f:
+        f.writelines(Correspondence_list)
+    return 110
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filename', type=str, required=True,
@@ -274,13 +320,17 @@ def main():
     parser.add_argument('-s', '--sr', type=int, default=24000,
                         help='sampling rate (default = 24000)')
     parser.add_argument('-t', '--target', type=int, default=9999,
-                        help='pre_traind targetid (zundamon = 100, sora = 101, methane = 102)')
+                        help='pre_traind targetid (zundamon = 100, sora = 101, methane = 102, tsumugi = 103)')
+    parser.add_argument('-m', '--multi_target', type=str, default=None,
+                        help='pre_traind targetid (zundamon = 100, sora = 101, methane = 102, tsumugi = 103)')
     parser.add_argument('-c', '--config', type=str, default="./configs/baseconfig.json",
                         help='JSON file for configuration')
     args = parser.parse_args()
     filename = args.filename
     print(filename)
-    if args.target != 9999 and args.target == 100:
+    if args.multi_target != None:
+        n_spk = create_dataset_multi_character(filename, args.multi_target)
+    elif args.target != 9999 and args.target == 100:
         n_spk = create_dataset_zundamon(filename)
     elif args.target != 9999:
         n_spk = create_dataset_character(filename, args.target)
